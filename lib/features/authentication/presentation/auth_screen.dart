c88e990c161efbcc46dc324ad37f081150a80d3a
Afeef K K <45afeef@gmail.com>
2024-05-19 06:38:33 +0530

    added landscape support

--- lib/features/authentication/presentation/auth_screen.dart
+++ lib/features/authentication/presentation/auth_screen.dart
@@ -7,115 +7,145 @@ class AuthScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.network(
-            'https://img.freepik.com/free-vector/training-home-concept_23-2148479237.jpg',
-            color: Theme.of(context).colorScheme.background,
-            colorBlendMode: BlendMode.multiply,
-          ).animate().fade(),
-          Text(
-            'Welcome!',
+      child: isPortrait
+          ? _buildPortraitView(context)
+          : _buildLandscapeView(context),
+    );
+  }
+
+  Row _buildLandscapeView(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        _leftSideSection(context),
+        Expanded(flex: 1, child: Column(children: _rightSideSection(context)))
+      ],
+    );
+  }
+
+  Column _buildPortraitView(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        _leftSideSection(context),
+        ..._rightSideSection(context),
+      ],
+    );
+  }
+
+  Widget _leftSideSection(BuildContext context) {
+    return Image.network(
+      'https://img.freepik.com/free-vector/training-home-concept_23-2148479237.jpg',
+      color: Theme.of(context).colorScheme.background,
+      colorBlendMode: BlendMode.multiply,
+    ).animate().fade();
+  }
+
+  List<Widget> _rightSideSection(BuildContext context) {
+    return <Widget>[
+      Text(
+        'Welcome!',
+        style: Theme.of(context)
+            .textTheme
+            .headlineMedium
+            ?.copyWith(fontWeight: FontWeight.bold),
+      ).animate().fade(delay: 300.milliseconds),
+      const Gap(12),
+      RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+            text: 'Connect with us and explore all the \n',
             style: Theme.of(context)
                 .textTheme
-                .headlineMedium
-                ?.copyWith(fontWeight: FontWeight.bold),
-          ).animate().fade(delay: 300.milliseconds),
-          const Gap(12),
-          RichText(
-            textAlign: TextAlign.center,
-            text: TextSpan(
-                text: 'Connect with us and explore all the \n',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium
-                    ?.copyWith(color: Colors.black38),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: "excellent benifits",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.amber[700]),
-                  ),
-                  const TextSpan(
-                    text: " of proper workout",
-                  ),
-                ]),
-          ).animate().fade(delay: 500.milliseconds),
-          const Gap(60),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
+                .bodyMedium
+                ?.copyWith(color: Colors.black38),
+            children: <TextSpan>[
+              TextSpan(
+                text: "excellent benifits",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.amber[700]),
               ),
-              minimumSize: const Size(260, 40),
-              backgroundColor: Colors.black,
-              foregroundColor: Colors.white,
-            ),
-            onPressed: () {},
-            child: const Text('Login'),
-          ).animate().fade(delay: 700.milliseconds),
-          TextButton(
-            style: TextButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                foregroundColor: Colors.black,
-                minimumSize: const Size(260, 40)),
-            onPressed: () {},
-            child: const Text('Sign Up'),
-          ).animate().fade(delay: 900.milliseconds),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const Expanded(
-                child: Divider(
-                  thickness: 1,
-                  color: Colors.grey,
-                ),
-              ).animate().scaleX(delay: 1.seconds),
-              const Gap(12),
-              const Icon(
-                Icons.star_border_purple500_rounded,
-                color: Colors.grey,
-              ).animate().scale(delay: 1200.milliseconds),
-              const Gap(12),
-              const Text('OR').animate().fade(delay: 1.seconds),
-              const Gap(12),
-              const Icon(
-                Icons.star_border_purple500_rounded,
-                color: Colors.grey,
-              ).animate().scale(delay: 800.milliseconds),
-              const Gap(12),
-              Expanded(
-                child: const Divider(
-                  thickness: 1,
-                  color: Colors.grey,
-                ).animate().scaleX(delay: 1.seconds),
+              const TextSpan(
+                text: " of proper workout",
               ),
-            ],
+            ]),
+      ).animate().fade(delay: 500.milliseconds),
+      const Gap(60),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
           ),
-          const Gap(16),
-          Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SocialButton(
-                onPressed: () {},
-                child: Image.asset(
-                  'assets/images/google.png',
-                  width: 24,
-                  height: 24,
-                ),
-              ).animate().fade(delay: 1.5.seconds),
-            ],
+          minimumSize: const Size(260, 40),
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        onPressed: () {},
+        child: const Text('Login'),
+      ).animate().fade(delay: 700.milliseconds),
+      TextButton(
+        style: TextButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            foregroundColor: Colors.black,
+            minimumSize: const Size(260, 40)),
+        onPressed: () {},
+        child: const Text('Sign Up'),
+      ).animate().fade(delay: 900.milliseconds),
+      Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const Expanded(
+            child: Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ),
+          ).animate().scaleX(delay: 1.seconds),
+          const Gap(12),
+          const Icon(
+            Icons.star_border_purple500_rounded,
+            color: Colors.grey,
+          ).animate().scale(delay: 1200.milliseconds),
+          const Gap(12),
+          const Text('OR').animate().fade(delay: 1.seconds),
+          const Gap(12),
+          const Icon(
+            Icons.star_border_purple500_rounded,
+            color: Colors.grey,
+          ).animate().scale(delay: 800.milliseconds),
+          const Gap(12),
+          Expanded(
+            child: const Divider(
+              thickness: 1,
+              color: Colors.grey,
+            ).animate().scaleX(delay: 1.seconds),
           ),
         ],
       ),
-    );
+      const Gap(16),
+      Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SocialButton(
+            onPressed: () {},
+            child: Image.asset(
+              'assets/images/google.png',
+              width: 24,
+              height: 24,
+            ),
+          ).animate().fade(delay: 1.5.seconds),
+        ],
+      ),
+    ];
   }
 }
 
